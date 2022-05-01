c280dd8795a723d27eb309b801c3ee8060792e3b
kayma <kayma@hlag.com>
2022-05-01 20:06:03 +0200

    feat: add classes to create user defined scanners

--- src/java/main/com/hlag/tools/commvis/analyzer/model/AbstractCommunicationModelVisitor.java
+++ src/java/main/com/hlag/tools/commvis/analyzer/model/AbstractCommunicationModelVisitor.java
@@ -1,11 +1,11 @@
 package com.hlag.tools.commvis.analyzer.model;
 
 /**
- * A visitor which processes the {@link AbstractCommunicationModel}.
+ * A visitor which processes the {@link CommunicationModel}.
  */
 public abstract class AbstractCommunicationModelVisitor {
-    public abstract void visit(AbstractCommunicationModel model);
+    public abstract void visit(CommunicationModel model);
 
-    public abstract void visit(IHttpReceiver httpReceiver);
-    public abstract void visit(IJmsReceiver jmsReceiver);
+    public abstract void visit(HttpReceiver httpReceiver);
+    public abstract void visit(JmsReciever jmsReceiver);
 }
--- src/java/main/com/hlag/tools/commvis/analyzer/model/ISenderReceiverCommunication.java
+++ src/java/main/com/hlag/tools/commvis/analyzer/model/ISenderReceiverCommunication.java
@@ -4,4 +4,5 @@ package com.hlag.tools.commvis.analyzer.model;
  * The top level interface for all producers and consumers or senders and receivers.
  */
 public interface ISenderReceiverCommunication {
+    void visit(AbstractCommunicationModelVisitor visitor);
 }
