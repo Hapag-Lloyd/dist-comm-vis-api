e16f13285f4d846258897553ced38fe8f2977d94
Matthias Kay <kayma@hlag.com>
2022-05-12 12:33:13 +0200

    chore: decouple the internal model names from the generated JSON (#14)

--- src/main/java/com/hlag/tools/commvis/analyzer/model/HttpProducer.java
+++ src/main/java/com/hlag/tools/commvis/analyzer/model/HttpProducer.java
@@ -1,5 +1,6 @@
 package com.hlag.tools.commvis.analyzer.model;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Value;
 
 /**
@@ -10,23 +11,31 @@ public class HttpProducer implements ISenderReceiverCommunication {
     /**
      * The full qualified classname where the producer lives.
      */
+    @SerializedName(value="className")
     private final String className;
+
     /**
      * The method name of the producer.
      */
+    @SerializedName(value="methodName")
     private final String methodName;
 
     /**
      * The HTTP method like GET or POST.
      */
+    @SerializedName(value="type")
     private final String type;
+
     /**
      * The full path name called (usually without a domain).
      */
+    @SerializedName(value="path")
     private final String path;
+
     /**
      * The project id of the referenced project.
      */
+    @SerializedName(value="destinationProjectId")
     private final String destinationProjectId;
 
     @Override
---
+++ src/test/java/com/hlag/tools/commvis/analyzer/model/JmsReceiverTest.java
@@ -0,0 +1,21 @@
+package com.hlag.tools.commvis.analyzer.model;
+
+import com.google.gson.annotations.SerializedName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JmsReceiverTest {
+    @Test
+    void shouldHaveSerializedNameAnnotationOnFiled_toDecoupleTheFieldNameFromJson() {
+        Field[] declaredFields = CommunicationModel.class.getDeclaredFields();
+
+        for (Field f : declaredFields) {
+            SerializedName actualAnnotation = f.getAnnotation(SerializedName.class);
+
+            assertThat(actualAnnotation).isNotNull();
+        }
+    }
+}
