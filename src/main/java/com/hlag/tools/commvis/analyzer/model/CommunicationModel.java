c0df28e487be753303952066d15181922130d3b3
kayma <kayma@hlag.com>
2022-05-12 20:14:29 +0200

    feat: add version attribute to model

--- src/main/java/com/hlag/tools/commvis/analyzer/model/CommunicationModel.java
+++ src/main/java/com/hlag/tools/commvis/analyzer/model/CommunicationModel.java
@@ -3,9 +3,11 @@ package com.hlag.tools.commvis.analyzer.model;
 import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Base class for the entire communication model, e.g. senders and receivers.
@@ -14,42 +16,53 @@ import java.util.Collection;
  * interface.
  */
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @ToString
 public class CommunicationModel {
     /**
      * Identifier for the current project, e.g. gitlab project id
      */
     @SerializedName(value="project_id")
-
-    private String projectId;
+    private final String projectId;
 
     /**
      * A name for the project. Just for information.
      */
-
     @SerializedName(value="project_name")
-    private String projectName;
+    private final String projectName;
+
+    @SerializedName(value="model_version")
+    private final String modelVersion;
 
     /**
      * All HTTP receiver endpoints.
      */
     @SerializedName(value="http_consumers")
-    private Collection<ISenderReceiverCommunication> httpConsumers;
+    private Collection<HttpConsumer> httpConsumers = new HashSet<>();
 
     /**
      * All HTTP producers.
      */
     @SerializedName(value="http_producers")
-    private Collection<ISenderReceiverCommunication> httpProducers;
+    private Collection<HttpProducer> httpProducers = new HashSet<>();
 
     /**
      * All JMS receivers.
      */
-
     @SerializedName(value="jms_consumers")
-    private Collection<ISenderReceiverCommunication> jmsConsumers;
+    private Collection<JmsReceiver> jmsConsumers = new HashSet<>();
 
+    public void addHttpConsumer(HttpConsumer consumer) {
+        httpConsumers.add(consumer);
+    }
+
+    public void addHttpProducer(HttpProducer producer) {
+        httpProducers.add(producer);
+    }
+
+    public void addJmsConsumer(JmsReceiver consumer) {
+        jmsConsumers.add(consumer);
+    }
 
     public void visit(AbstractCommunicationModelVisitor visitor) {
         visitor.visit(this);
