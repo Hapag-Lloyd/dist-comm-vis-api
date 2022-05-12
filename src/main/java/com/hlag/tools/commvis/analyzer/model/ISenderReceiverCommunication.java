a844b28ac1fab8681117465194969a482c65d5a5
Matthias Kay <kayma@hlag.com>
2022-05-12 21:37:44 +0200

    feat: add identifier to all endpoints (#19)

--- src/main/java/com/hlag/tools/commvis/analyzer/model/ISenderReceiverCommunication.java
+++ src/main/java/com/hlag/tools/commvis/analyzer/model/ISenderReceiverCommunication.java
@@ -4,5 +4,10 @@ package com.hlag.tools.commvis.analyzer.model;
  * The top level interface for all producers and consumers or senders and receivers.
  */
 public interface ISenderReceiverCommunication {
+    /**
+     * @return an identifier valid within a model
+     */
+    Long getId();
+
     void visit(AbstractCommunicationModelVisitor visitor);
 }
