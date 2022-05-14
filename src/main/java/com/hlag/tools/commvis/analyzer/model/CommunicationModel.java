512858c1d423fe8e856e21d607acefe990eccba8
kayma <kayma@hlag.com>
2022-05-14 11:29:18 +0200

    Use concrete classes

--- src/main/java/com/hlag/tools/commvis/analyzer/model/CommunicationModel.java
+++ src/main/java/com/hlag/tools/commvis/analyzer/model/CommunicationModel.java
@@ -1,10 +1,7 @@
 package com.hlag.tools.commvis.analyzer.model;
 
 import com.google.gson.annotations.SerializedName;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -39,27 +36,34 @@ public class CommunicationModel {
      * All HTTP receiver endpoints.
      */
     @SerializedName(value="http_consumers")
-    private Collection<ISenderReceiverCommunication> httpConsumers = new HashSet<>();
+    private Collection<HttpConsumer> httpConsumers = new HashSet<>();
 
     /**
      * All HTTP producers.
      */
     @SerializedName(value="http_producers")
-    private Collection<ISenderReceiverCommunication> httpProducers = new HashSet<>();
+    private Collection<HttpProducer> httpProducers = new HashSet<>();
 
     /**
      * All JMS receivers.
      */
     @SerializedName(value="jms_consumers")
-    private Collection<ISenderReceiverCommunication> jmsConsumers = new HashSet<>();
+    private Collection<JmsReceiver> jmsConsumers = new HashSet<>();
+
+    private CommunicationModel() {
+        // for GSON deserialize
+        projectId = "not-set";
+        projectName = "not-set";
+        modelVersion = "not-set";
+    }
 
     public <T extends ISenderReceiverCommunication> void addSenderReceiver(T endpoint) {
         if (endpoint instanceof HttpConsumer) {
-            httpConsumers.add(endpoint);
+            httpConsumers.add((HttpConsumer) endpoint);
         } else if (endpoint instanceof HttpProducer) {
-            httpProducers.add(endpoint);
+            httpProducers.add((HttpProducer) endpoint);
         } else if (endpoint instanceof JmsReceiver) {
-            jmsConsumers.add(endpoint);
+            jmsConsumers.add((JmsReceiver) endpoint);
         } else {
             throw new IllegalStateException(String.format("We have no endpoints of type %s", endpoint.getClass().getCanonicalName()));
         }
